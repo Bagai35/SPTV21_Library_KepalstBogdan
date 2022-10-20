07c298aea5df9be992cfcf8f1bb4d3c5bc7db623
Bagai35 <bogdan.keplast@gmail.com>
2022-10-20 08:27:00 +0300

    добвление чтения и изменения книг и авторов

--- src/Entity/Book.java
+++ src/Entity/Book.java
@@ -1,10 +1,5 @@
-
-
 package Entity;
-
 import java.util.Arrays;
-
-
 public class Book {
     private String bookName;
     private Author[] authors;
@@ -30,17 +25,32 @@ public class Book {
     public void setAuthors(Author[] authors) {
         this.authors = authors;
     }
-    
+
     public void addAuthor(Author author){
         Author[] newAuthors = Arrays.copyOf(authors, authors.length+1);
         newAuthors[newAuthors.length - 1] = author;
         authors = newAuthors;
     }
-    
+
     public int getPublishedYear() {
         return publishedYear;
     }
-
+    public void removeAuthor(int numberOfAuthor){
+        //обнуляем указанного автора (по индексу)
+        this.getAuthors()[numberOfAuthor-1]=null;
+        //создаем массив с количеством элементов на 1 меньше
+        Author[] newAuthors = new Author[this.getAuthors().length-1];
+        // в цикле копируем элементы в новый массив не учитывая обнуленную ячейку
+        int j = 0;
+        for (Author author : this.getAuthors()) {
+            if (author != null) {
+                newAuthors[j] = author;
+                j++;
+            }
+        }
+        //копируем ссылку на новый массив в книгу
+        this.setAuthors(newAuthors);
+    }
     public void setPublishedYear(int publishedYear) {
         this.publishedYear = publishedYear;
     }
@@ -55,12 +65,12 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book{" 
-                + "bookName=" + bookName 
+        return "Book{"
+                + "bookName=" + bookName
                 + ", authors=" + Arrays.toString(authors)
-                + ", publishedYear=" + publishedYear 
-                + ", quantity=" + quantity 
+                + ", publishedYear=" + publishedYear
+                + ", quantity=" + quantity
                 + '}';
     }
-    
+
 }
